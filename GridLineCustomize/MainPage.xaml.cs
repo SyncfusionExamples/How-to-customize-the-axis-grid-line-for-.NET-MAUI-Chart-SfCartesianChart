ae5829db0944ac9020475c59ac87bc5b34e3e4f5
EzhilarasanSF4095 <ezhilarasan.sf4095@gmail.com>
2023-06-26 19:42:48 +0530

    Updated sample

---
+++ GridLineCustomize/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GridLineCustomize;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
--- Grid_line_customize/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace Grid_line_customize;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
